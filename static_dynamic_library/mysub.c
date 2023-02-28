01067f593015903b4e1c00ed81180b1a889c6e2f
lijinggai <2786810048@qq.com>
2023-02-28 23:51:57 +0800

    static_dynamic_library/

---
+++ static_dynamic_library/mysub.c
@@ -0,0 +1,6 @@
+#include"mysub.h"
+int Sub(int a,int b)
+{
+	return a-b;
+}
+
---
+++ static_dynamic_library/test.c
@@ -0,0 +1,9 @@
+#include"mysub.h"
+#include"myadd.h"
+int main()
+{
+	int r1=Sub(5,1);
+	int r2=Add(5,1);
+	printf("r1=%d,r2=%d\n",r1,r2);
+	return 0;
+}
